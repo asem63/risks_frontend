d91bf95fdd9ce74e0fa4d44c70d1e3bcc244b7d7
Asem63 <asem63@gmail.com>
2014-03-22 21:00:26 +0200

    company added

--- app.js
+++ app.js
@@ -38,6 +38,7 @@ if ('development' == app.get('env')) {
 
 app.get('/', routes.index);
 app.get('/js/api', routes.api);
+app.get('/company', routes.company);
 app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
--- routes/index.js
+++ routes/index.js
@@ -32,3 +32,55 @@ exports.api = function(req, res){
     res.contentType('application/json');
     res.send(JSON.stringify(result));
 };
+
+exports.company = function(req, res){
+    var url_parts = url.parse(req.url, true);
+    var query = url_parts.query.name;
+    console.log("name:"+query);
+    company_data =
+    {
+        name: "random_name",
+        info:[
+            {
+                year:2010,
+                k:{
+                    k1: 1.2,
+                    k2: 3,
+                    k3: 0.5,
+                    k4: 1.5,
+                    k5: 1,
+                    k6: 0.8,
+                    k7: 2,
+                    k8: 1,
+                    k9: 3,
+                    k10: 0.01,
+                    k11: 0.5,
+                    k12: 0.9,
+                    k13: 2,
+                    mark: 90
+                }
+            },
+            {
+                year:2011,
+                k:{
+                    k1: 1.1,
+                    k2: 3,
+                    k3: 0.5,
+                    k4: 1.3,
+                    k5: 1,
+                    k6: 0.5,
+                    k7: 1,
+                    k8: 1,
+                    k9: 2,
+                    k10: 0.01,
+                    k11: 0.4,
+                    k12: 0.7,
+                    k13: 1,
+                    mark: 80
+                }
+            },
+        ]
+    }
+
+    res.render('company', { data: company_data });
+};
