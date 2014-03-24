82ce85efc969341df690416023b13dfce5ef92a2
Asem63 <asem63@gmail.com>
2014-03-24 02:25:17 +0200

    small fixes

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,8 @@ var url = require('url');
 var _ = require('underscore');
 var request = require('request');
 
-var url_company_names = "http://example.com/names";
-var url_company = "http://example.com/company?";
-//var url_test = "http://developer.cumtd.com/api/v2.2/json/GetStop?" +
-//    "key=d99803c970a04223998cabd90a741633" +
-//    "&stop_id=it"
+var url_company_names = "http://finance-kpirisk.rhcloud.com/rest/risk/corporations";
+var url_company = "http://finance-kpirisk.rhcloud.com/rest/risk/data/";
 
 var names = [];
 
@@ -18,9 +15,12 @@ exports.index = function(req, res){
 
         if (!error && response.statusCode === 200) {
             console.log(body) // Print the json response
-            names = body;
+            for (index = 0; index < body.length; ++index) {
+                names.push({value:body[index]});
+            }
         }
     })
+
     res.render('index', { test: 'Express' });
 };
 
@@ -29,12 +29,9 @@ exports.api = function(req, res){
 
     var url_parts = url.parse(req.url, true);
     var query = url_parts.query.name;
-    console.log("QUERY:"+query);
-    console.log(names)
-    result = _.filter(names, function(el){
+    var result = _.filter(names, function(el){
         return (el.value.indexOf(query) > -1)
     });
-    console.log(result)
 
     res.contentType('application/json');
     res.send(JSON.stringify(result));
@@ -47,7 +44,7 @@ exports.company = function(req, res){
     var company_data = {};
 
     request({
-        url: url_company+"name="+c_name,
+        url: url_company+c_name,
         json: true
     }, function (error, response, body) {
 
@@ -58,7 +55,8 @@ exports.company = function(req, res){
             res.render('index', { test: 'Express' });
         }
     })
-    console.log("COMPANYDATA:"+company_data);
+    console.log("COMPANYDATA: %j",company_data);
+
 
     res.render('company', { data: company_data });
 };
